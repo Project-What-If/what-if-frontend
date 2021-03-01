23554c95bc5c59b981519b68febe86f2655fbfe1
SeungWookHan <hswook12@me.com>
2021-03-02 05:00:24 +0900

    Add: RoomForm data export logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import RoomForm from './components/RoomForm';
 
 function App() {
-    return <div>hello</div>;
+    const nextId = useRef(2);
+    const [rooms, setRooms] = useState([
+        {
+            id: 1,
+            title: '제목입니다',
+            content: '본문입니다.',
+            image: '사진입니다.',
+            imageURL: '',
+            tag: 'tag1, tag2',
+        },
+    ]);
+    const handleCreate = data => {
+        setRooms(rooms.concat({ id: nextId.current, ...data }));
+        nextId.current += 1;
+    };
+    return (
+        <div>
+            <RoomForm onCreate={handleCreate} />
+            {JSON.stringify(rooms)}
+        </div>
+    );
 }
 
 export default App;
--- src/components/RoomForm.js
+++ src/components/RoomForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function RoomForm(props) {
     const [inputs, setInputs] = useState({
